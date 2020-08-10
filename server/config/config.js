82c1bf5a5a71383e557cad00ad16151b35f20b74
jkcomt <jkcomt@gmail.com>
2020-08-09 20:59:41 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -24,8 +24,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB =
-        "mongodb+srv://strider:9RdfgjiJ77hM5PHT@cluster0.4vyjc.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
